3477f314cccb78d91a7293a2d0dc647500d7f477
ztan0107 <101960115+ztan0107@users.noreply.github.com>
2024-10-17 22:23:18 +0800

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -5,13 +5,13 @@
 
 //Line 2 embeds the Vega-Lite visualisation stored in “vg_1” in the div container named “bar_chart” (do not forget the “#” key before the div name).
 
-var vg_1 = "js/total_number_of_disasters_occur_each_year.vg.json";
+var vg_1 = "FIT3179 Assignment 2/js/natural_disasters_contributes_to_death.vg.json";
 vegaEmbed("line_chart", vg_1).then(function(result) {
 // Access the Vega view instance
 (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_2 = "js/natural_disasters_contributes_to_death.vg.json";
+var vg_2 = "FIT3179 Assignment 2/js/natural_disasters_contributes_to_death.vg.json";
 vegaEmbed("pie_chart", vg_1).then(function(result) {
 // Access the Vega view instance
 (https://vega.github.io/vega/docs/api/view/) as result.view
